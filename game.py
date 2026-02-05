6c4c7ec38eea997370d43d0e69981c40751710b7
kashishgada <kashishgada@gmail.com>
2026-02-05 18:08:14 +0100

    added comments for better readability

--- game.py
+++ game.py
@@ -1,10 +1,16 @@
+# generate a random number
 import random
 number_to_guess = random.randint(1 ,100)
 
+# While loop so that it deosnt end afetr one cycle
 while True:
+
+    # try for if the number is not valid
     try:
+        # ask the user to makea guess
         guess = int(input('please guess any mumber between 1 to 100:  '))
 
+        # if-elif-else loop for conditional stmts
         if guess < number_to_guess:
             print('number is too low')
 
@@ -13,6 +19,9 @@ while True:
 
         else:
             print('you guessed it right!')
-            break
+
+        # break to end while loop
+        break
+    
     except ValueError:
         print('pls enter a valid number')
